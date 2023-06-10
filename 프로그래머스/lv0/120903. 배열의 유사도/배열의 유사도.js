7771df0f6d3a28eb968ace354cf425df88938134
junyoung <114667764+zunyange@users.noreply.github.com>
2023-06-10 15:28:30 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,13 +1,13 @@
 function solution(s1, s2) {
-    /*var answer = s1.filter(x => s2.includes(x));
-        let result = answer.length;*/
-        var answer = 0;
-        for(var i=0; i<s1.length; i++) {
-            for(var j=0; j<s2.length; j++) {
-            if(s1[i] === s2[j]) 
-                answer++;
-            }
-        }
-    return answer;
+    var answer = s1.filter(x => s2.includes(x));
+        return answer.length;
+    //     var answer = 0;
+    //     for(var i=0; i<s1.length; i++) {
+    //         for(var j=0; j<s2.length; j++) {
+    //         if(s1[i] === s2[j]) 
+    //             answer++;
+    //         }
+    //     }
+    // return answer;
 
 }
