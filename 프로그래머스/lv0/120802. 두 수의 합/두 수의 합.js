456db883ebb2eb9ed1ae3b05840bbc4a4c21c3c5
junyoung <114667764+zunyange@users.noreply.github.com>
2023-06-23 23:55:24 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    var answer = 0;
+    if (num1>50000 || num1<-50000 && num2>5000 || num2<-50000) return;
+    else {return answer = num1 + num2;
+    }
+    return answer;
+}
