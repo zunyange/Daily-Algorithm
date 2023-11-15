6c37b8c8514e5d888f48c8a7bedb76c6140b0926
Junyoung <hiillun00553300@gmail.com>
2023-11-15 18:14:26 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers, n) {
+    var answer = 0;
+    
+    for (let i=0; i<numbers.length; i++) {
+    answer = answer + numbers[i]
+    if (answer > n) {
+        return answer;
+    }
+    }
+    
+    return answer;
+}
