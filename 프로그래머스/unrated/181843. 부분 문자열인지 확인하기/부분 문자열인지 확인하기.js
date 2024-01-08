4c8a771ca0d45e108f31e33dc1c0f8389b4f12bc
Junyoung <hiillun00553300@gmail.com>
2024-01-08 23:45:03 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -0,0 +1,10 @@
+function solution(my_string, target) {
+    var answer = 0;
+    
+    if(my_string.includes(target)){
+        return answer = 1;
+    }else{
+        return answer = 0;
+    }
+    return answer;
+}
