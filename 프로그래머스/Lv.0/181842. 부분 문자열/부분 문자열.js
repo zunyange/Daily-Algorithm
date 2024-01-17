a75b7d47964de8ae3644c15a4a57e448dd46ce6b
Junyoung <hiillun00553300@gmail.com>
2024-01-17 17:59:59 +0900

    [level Lv.0] Title: 부분 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181842. 부분 문자열/부분 문자열.js
@@ -0,0 +1,11 @@
+function solution(str1, str2) {
+    var answer = 0;
+    
+    if (str2.includes(str1)) {
+        answer = 1;
+    } else {
+        answer = 0;
+    }
+    
+    return answer;
+}
